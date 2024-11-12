a1336f62a33a78f6d4c833a3e24ebf1f991c1fad
arnugroho <nugroho.akbar.r@gmail.com>
2024-11-12 16:50:40 +0700

    bable config

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: 'css',
+      },
+      'antd', // Name of the plugin instance
+    ],
+  ],
+};
